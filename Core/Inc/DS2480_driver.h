26e9e5f7b627627a89cccac408a919dc03b93a48
Drawell <ivanevgnikonov@gmail.com>
2020-12-20 17:34:16 +0300

    So it begins

---
+++ Core/Inc/DS2480_driver.h
@@ -0,0 +1,15 @@
+#ifndef DS2480_DRIVER_H
+#define DS2480_DRIVER_H
+#include "cmsis_os.h"
+
+
+osMessageQDef(DS2480_MQ, 1, uint8_t); // Define message queue
+osMessageQId  DS2480_MQ;
+
+void DS2480_DriverIni();
+
+void DS2480_Response(uint8_t responce_buffer, uint16_t size, uint16_t timeout);
+
+
+
+#endif
---
+++ Core/Src/DS2480_driver.c
@@ -0,0 +1,52 @@
+/*
+ * DS2480_driver.c
+ *
+ *  Created on: 20 дек. 2020 г.
+ *      Author: Nikon
+ */
+#include "DS2480_driver.h"
+
+const uint16_t buf_size = 255;
+static uint8_t buf[buf_size];
+static uint16_t write_ptr = 0;
+static uint16_t read_ptr = 0;
+
+osThreadDef(ReceiveUartThread, osPriorityNormal, 1, 2000);
+
+void ReceiveUartThread(void const *argument);
+
+
+void DS2480_DriverIni()
+{
+	tid_thread1 = osThreadCreate(osThread(ReceiveUartThread), NULL);
+}
+
+void DS2480_Response(uint8_t responce_buffer, uint16_t size, uint16_t timeout)
+{
+	for (uint16_t i = 0; i < size; i++)
+	{
+		responce_buffer = buf[read_ptr];
+		read_ptr = (read_ptr + 1) % buf_size;
+	}
+
+}
+
+
+
+
+void ReceiveUartThread(void const *argument)
+{
+	osEvent evt;
+	uint8_t data;
+
+	while(1){
+		evt = osMessageGet(MsgBox, osWaitForever);  // wait for message
+		if (evt.status == osEventMessage) {
+			data = evt.value.p;
+			buf[write_ptr] = data;
+			write_ptr = (write_ptr + 1) % buf_size;
+		}
+	}
+
+}
+
